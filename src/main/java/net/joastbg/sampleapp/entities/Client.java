692dc72b0d034e4b561485b34e396c42ff57cc40
arnaudg <arnaudguilhaumon@gmail.com>
2018-05-03 17:13:39 +0200

    client et contact

--- src/main/java/net/joastbg/sampleapp/entities/Client.java
+++ src/main/java/net/joastbg/sampleapp/entities/Client.java
@@ -1,11 +1,40 @@
 package net.joastbg.sampleapp.entities;
 
+import org.hibernate.annotations.Entity;
 
+import javax.persistence.*;
+import java.util.List;
 
+/**
+ * Describes an Person
+ *
+ * @author Charlotte Cavalier <charlotte.cavalier@gmail.com>
+ */
+@Entity
+@SequenceGenerator(name = "seq", initialValue = 1)
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Client {
 
-public class Client  {
+    @Id
+    @Column(name = "idClient")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "seq")
+    private Long id;
 
+    @Column(name = "nom")
+    private String nom;
+
+    @Column(name = "comptePrincipal")
+    private String comptePrincipal;
+
+    @OneToMany
+    @JoinTable(
+            name = "COMPTE_CLIENT",
+            joinColumns = {@JoinColumn(name = "idClient")},
+            inverseJoinColumns = {@JoinColumn(name = "idClient")}
+    )
+    private List<CompteBancaire> compteBancaireList;
+
+    public Client() {
+
+    }
 }
-
-
-
--- src/main/java/net/joastbg/sampleapp/entities/Contact.java
+++ src/main/java/net/joastbg/sampleapp/entities/Contact.java
@@ -1,4 +1,27 @@
 package net.joastbg.sampleapp.entities;
 
-public class Contact {
+import org.hibernate.annotations.Entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@SequenceGenerator(name="seq", initialValue=1)
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Contact{
+
+    @Id
+    @Column(name = "idContact")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator="seq")
+    private Long id;
+
+    @Column(name = "typeContact")
+    private String typeContact;
+
+    @Column(name = "valeurContact")
+    private String valeurContact;
+
+    public Contact() {
+
+    }
 }
