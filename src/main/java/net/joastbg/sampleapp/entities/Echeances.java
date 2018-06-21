f437d6200b9c7d6315d9922c72be0ee8250528ce
Clément Correia <clement.correia@avizzeo.com>
2018-06-21 11:28:37 +0200

    assurance + echeance

--- src/main/java/net/joastbg/sampleapp/entities/Echeances.java
+++ src/main/java/net/joastbg/sampleapp/entities/Echeances.java
@@ -1,4 +1,31 @@
 package net.joastbg.sampleapp.entities;
 
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "COMPTE_BANCAIRE")
 public class Echeances {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "idEcheance")
+    private Long id;
+
+    @Column
+    private int prix;
+
+    @Column
+    private Date dateEmission;
+
+    @Column
+    private Date datePaiement;
+
+    @Column
+    private Date dateEmissionFacture;
+
+    @ManyToOne
+    @JoinColumn(name = "idAssurance")
+    private Assurance assurance;
+
 }
